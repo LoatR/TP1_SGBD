5a87799c1d2c6e34e992c0a3d37ca8fabc763613
AndyBouh <andrea.lourenco@etu.parisdescartes.fr>
2017-11-16 10:06:01 +0100

    tests

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -13,24 +13,30 @@ public class Main {
 		// LRU:6, FIFO:5, CLOCK:6
 		// FIFO:5, LRU:6, CLOCK:6
 		
-		String listToReadTest3[] ={"",""};
+		String listToReadTest3[] ={"B", "C", "B", "A", "D", "C", "E", "A"};
+		// LRU:5, FIFO:5, CLOCK:6
+		// FIFO:5, LRU:5, CLOCK:6
+		
+		String listToReadTest4[] ={"B", "C", "B", "A", "E", "B", "D", "C", "B", "D","E", "B", "E", "C", "C", "G", "E"};
+		// LRU:7, FIFO:9, CLOCK:8
+		// LRU:7, CLOCK:8, FIFO:9
 		
 
 		System.out.println("--------------- LRU ------------------");
-		for (int i = 0; i < listToReadTest1.length; i++) {
-			bl.read(listToReadTest1[i]);
+		for (int i = 0; i < listToReadTest4.length; i++) {
+			bl.read(listToReadTest4[i]);
 			System.out.println(bl);
 		}
 
 		System.out.println("--------------- FIFO ------------------");
-		for (int i = 0; i < listToReadTest1.length; i++) {
-			bf.read(listToReadTest1[i]);
+		for (int i = 0; i < listToReadTest4.length; i++) {
+			bf.read(listToReadTest4[i]);
 			System.out.println(bf);
 		}
 
 		System.out.println("--------------- CLOCK ------------------");
-		for (int i = 0; i < listToReadTest1.length; i++) {
-			bc.read(listToReadTest1[i]);
+		for (int i = 0; i < listToReadTest4.length; i++) {
+			bc.read(listToReadTest4[i]);
 			System.out.println(bc);
 		}
 
